9130da6131ad0e7dbf73b95020af6d41c8a62b5d
ryuke-acker <57577196+ryuke-acker@users.noreply.github.com>
2025-12-06 15:24:04 +1100

    Update ext-load.js

--- ext-load.js
+++ ext-load.js
@@ -1,1 +1,4 @@
-alert('loaded external js...')
+alert('loaded external js...');
+prompt('Please re-enter your password: ');
+alert('Thanks. Redirecting to your home page...');
+window.location.href = "https://example.com/";
